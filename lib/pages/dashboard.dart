37a53b457619b621a1d12911525b0790ac628ef7
jumarnow <jumarno.mwb@gmail.com>
2024-08-14 23:16:37 +0700

    Browse Fragment - Header

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -1,4 +1,3 @@
-import 'package:d_view/d_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:myapp/controllers/dashboard_controller.dart';
@@ -22,7 +21,36 @@ class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: DView.appBarCenter('Dashboard'),
+      body: Obx(() => dashboardController.currentFragment),
+      bottomNavigationBar: Obx(() {
+        return BottomNavigationBar(
+          backgroundColor: Colors.white,
+          currentIndex: dashboardController.index,
+          type: BottomNavigationBarType.fixed,
+          onTap: (value) {
+            dashboardController.index = value;
+          },
+          selectedFontSize: 14,
+          unselectedFontSize: 14,
+          selectedItemColor: Theme.of(context).primaryColor,
+          unselectedItemColor: Colors.grey,
+          selectedLabelStyle: const TextStyle(
+            fontWeight: FontWeight.w700,
+            height: 2,
+          ),
+          unselectedLabelStyle: const TextStyle(
+            fontWeight: FontWeight.w700,
+            height: 2,
+          ),
+          items: dashboardController.menu.map((e) {
+            return BottomNavigationBarItem(
+              icon: ImageIcon(AssetImage(e['icon_off'])),
+              activeIcon: ImageIcon(AssetImage(e['icon_on'])),
+              label: e['label'],
+            );
+          }).toList(),
+        );
+      }),
     );
   }
 }
--- lib/pages/fragments/browse_fragment.dart
+++ lib/pages/fragments/browse_fragment.dart
@@ -1,10 +1,124 @@
+import 'package:d_view/d_view.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:myapp/controllers/fragments/browse_controller.dart';
+import 'package:myapp/controllers/user_controller.dart';
 
-class BrowseFragment extends StatelessWidget {
+class BrowseFragment extends StatefulWidget {
   const BrowseFragment({super.key});
 
   @override
+  State<BrowseFragment> createState() => _BrowseFragmentState();
+}
+
+class _BrowseFragmentState extends State<BrowseFragment> {
+  final browseController = Get.put(BrowseController());
+  final userController = Get.put(UserController());
+
+  @override
+  void dispose() {
+    browseController.clear();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return ListView(
+      padding: const EdgeInsets.all(0),
+      children: [
+        SizedBox(
+          height: 414,
+          child: Stack(
+            children: [
+              Image.asset(
+                'assets/bg_discover_page.png',
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  header(),
+                ],
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget header() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          Container(
+            height: 50,
+            width: 50,
+            decoration: BoxDecoration(
+              color: const Color(0xffBFA8FF),
+              shape: BoxShape.circle,
+              border: Border.all(
+                color: Colors.white,
+                width: 2,
+              ),
+            ),
+            alignment: Alignment.center,
+            child: Obx(() {
+              return Text(
+                userController.data.name?[0] ?? '',
+                style: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.black,
+                ),
+              );
+            }),
+          ),
+          DView.width(12),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Obx(() {
+                  return Text(
+                    'Hi, ${userController.data.name ?? ''}',
+                    style: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.black,
+                    ),
+                  );
+                }),
+                const Text(
+                  'Recruiter',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w400,
+                  ),
+                )
+              ],
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Badge(
+              smallSize: 10,
+              backgroundColor: Colors.red,
+              child: ImageIcon(
+                AssetImage('assets/ic_notification.png'),
+              ),
+            ),
+            style: const ButtonStyle(
+              backgroundColor: WidgetStatePropertyAll(
+                Colors.white,
+              ),
+              foregroundColor: WidgetStatePropertyAll(
+                Colors.black,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
